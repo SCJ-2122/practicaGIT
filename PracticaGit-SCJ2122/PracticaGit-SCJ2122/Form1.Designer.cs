bc092edeac35dd77ad1950416799c342639cc335
SCJ2122 <carpena22041@iesmarenostrum.com>
2022-02-27 01:22:43 +0100

    Tercera version del proyecto: se cambia textBox por numericUpDown

--- PracticaGit-SCJ2122/PracticaGit-SCJ2122/Form1.Designer.cs
+++ PracticaGit-SCJ2122/PracticaGit-SCJ2122/Form1.Designer.cs
@@ -31,11 +31,12 @@ namespace PracticaGit_SCJ2122
         {
             this.button1 = new System.Windows.Forms.Button();
             this.aluNombre = new System.Windows.Forms.TextBox();
-            this.aluNota = new System.Windows.Forms.TextBox();
             this.listaAlumnos = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.aluNota = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -54,13 +55,6 @@ namespace PracticaGit_SCJ2122
             this.aluNombre.Name = "aluNombre";
             this.aluNombre.Size = new System.Drawing.Size(333, 20);
             this.aluNombre.TabIndex = 1;
-            // 
-            // aluNota
-            // 
-            this.aluNota.Location = new System.Drawing.Point(405, 45);
-            this.aluNota.Name = "aluNota";
-            this.aluNota.Size = new System.Drawing.Size(100, 20);
-            this.aluNota.TabIndex = 2;
             // 
             // listaAlumnos
             // 
@@ -84,7 +78,7 @@ namespace PracticaGit_SCJ2122
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(405, 26);
+            this.label2.Location = new System.Drawing.Point(396, 29);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(30, 13);
             this.label2.TabIndex = 5;
@@ -99,21 +93,29 @@ namespace PracticaGit_SCJ2122
             this.label3.TabIndex = 6;
             this.label3.Text = "Lista de alumnos";
             // 
+            // aluNota
+            // 
+            this.aluNota.Location = new System.Drawing.Point(399, 45);
+            this.aluNota.Name = "aluNota";
+            this.aluNota.Size = new System.Drawing.Size(51, 20);
+            this.aluNota.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(688, 330);
+            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listaAlumnos);
-            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.aluNombre);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,11 +125,11 @@ namespace PracticaGit_SCJ2122
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox aluNombre;
-        private System.Windows.Forms.TextBox aluNota;
         private System.Windows.Forms.TextBox listaAlumnos;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown aluNota;
     }
 }
 
